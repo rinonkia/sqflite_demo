bd2adc57e33a5fe84f999ddc4efb259509d2b10e
rinonkia <git@gatapon.net>
2020-08-29 16:37:29 +0900

    change: rename class to InputTextRepository

--- lib/db/input_text_repository.dart
+++ lib/db/input_text_repository.dart
@@ -1,12 +1,17 @@
 import 'package:sqflite_demo/db/db_provider.dart';
 import 'package:sqflite_demo/model/input_text.dart';
 
-class TweetDraftRepository {
+class InputTextRepository {
   static String table = 'input_text';
   static DBProvider instance = DBProvider.instance;
 
-  Future<InputText> create(Map<String, dynamic> row,
-      {body, String createdAt}) async {
+  static Future<InputText> create(String text) async {
+    final Map<String, dynamic> row = {
+      'body': text,
+      'created_at': 'test',
+      'updated_at': 'test',
+    };
+
     final db = await instance.database;
     final id = await db.insert(table, row);
 
@@ -18,7 +23,7 @@ class TweetDraftRepository {
     );
   }
 
-  Future<List<InputText>> getAll() async {
+  static Future<List<InputText>> getAll() async {
     final db = await instance.database;
     final rows =
         await db.rawQuery('SELECT * FROM $table ORDER BY updated_at DESC');
@@ -27,7 +32,7 @@ class TweetDraftRepository {
     return rows.map((e) => InputText.fromMap(e)).toList();
   }
 
-  Future<InputText> single(int id) async {
+  static Future<InputText> single(int id) async {
     final db = await instance.database;
     final rows = await db.rawQuery('SELECT * FROM $table WHERE id = ?', [id]);
     if (rows.isEmpty) return null;
@@ -35,7 +40,7 @@ class TweetDraftRepository {
     return InputText.fromMap(rows.first);
   }
 
-  Future<int> delete(int id) async {
+  static Future<int> delete(int id) async {
     final db = await instance.database;
     return db.delete(table, where: 'id = ?', whereArgs: [id]);
   }
